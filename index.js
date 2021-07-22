385cae758d630ae7409f19b3a2f2a503a1a734ae
Leandro Murgo <leandrouni01@gmail.com>
2021-07-22 08:52:13 -0300

    feat(index.js) apply middlewares to parse data of request

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//Midlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
 app.listen(PORT, () => {
   console.log("Server listening on port: " + PORT);
 })
