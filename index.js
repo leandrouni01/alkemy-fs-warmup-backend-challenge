89572fcf01d8f61d3f3626ef70dfc03933b1dcdf
Leandro Murgo <leandrouni01@gmail.com>
2021-07-22 15:18:08 -0300

    feat(index.js) add post categories routes to app

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//Routes
+const postCategoryRoutes = require('./routes/postCategory');
 
+app.use('/post-categories', postCategoryRoutes);
 
 app.listen(PORT, () => {
   console.log("Server listening on port: " + PORT);
