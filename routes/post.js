85ac6162aa08def3bf074ef8f4be17dab0141f27
Leandro Murgo <leandrouni01@gmail.com>
2021-07-22 16:04:36 -0300

    feat(routes) create post routes

---
+++ routes/post.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const { findAll, findOne, create, update, remove } = require('../controllers/post');
+
+router.get('', findAll);
+
+router.get('/', findAll);
+router.get('/:id', findOne);
+router.post('', create);
+router.patch('/:id', update);
+router.delete('/:id', remove);
+
+module.exports = router;
