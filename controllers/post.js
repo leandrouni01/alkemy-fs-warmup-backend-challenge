9293924006fb4a4c384b7b582e44ff651b673f6f
Leandro Murgo <leandrouni01@gmail.com>
2021-07-22 16:03:48 -0300

    feat(controllers) create structure of post

---
+++ controllers/post.js
@@ -0,0 +1,21 @@
+const Post = require('../models/Post');
+
+exports.findAll = (req, res) => {
+
+}
+
+exports.findOne = (req, res) => {
+
+}
+
+exports.create = (req, res) => {
+
+}
+
+exports.update = (req, res) => {
+
+}
+
+exports.remove = (req, res) => {
+
+}
