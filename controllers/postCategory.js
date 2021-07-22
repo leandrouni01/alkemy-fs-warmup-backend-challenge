d1a2e32cfa664e37a499526582b81837df40f8e0
Leandro Murgo <leandrouni01@gmail.com>
2021-07-22 14:21:27 -0300

    feat(controllers) create post category controller structure

---
+++ controllers/postCategory.js
@@ -0,0 +1,19 @@
+exports.findAll = (req,res) => {
+
+}
+
+exports.findOne = (req,res) => {
+
+}
+
+exports.create = (req,res) => {
+
+}
+
+exports.update = (req,res) => {
+
+}
+
+exports.delete = (req,res) => {
+
+}
